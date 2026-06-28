1f0a5a5d45697d7e27669b481f405e228c60a928
highlevelnotes <pneuco153@gmail.com>
2026-06-28 17:44:34 +0900

    update all

--- codefactory_js/2_class_and_oop/2_getter_and_setter.js
+++ codefactory_js/2_class_and_oop/2_getter_and_setter.js
@@ -31,5 +31,26 @@ yuJin.setName = '장원영';
 console.log(yuJin);
 
 class IdolModel2{
-    
+    #name;
+    year;
+
+    constructor(name, year){
+        this.#name = name;
+        this.year = year;
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    set name(name){
+        this.#name = name;
+    }
 }
+
+const yuJin2 = new IdolModel2('안유진', 2003);
+console.log(yuJin2);
+console.log(yuJin2.name);
+
+yuJin2.name = '코드팩토리';
+console.log(yuJin2.name);
