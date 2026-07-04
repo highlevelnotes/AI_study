1739683a69f00b1afef675ec5468ced6239ac4d6
highlevelnotes <pneuco153@gmail.com>
2026-07-04 13:28:34 +0900

    update codefactory_js

--- codefactory_js/3_all_about_object/5_prototype_chain.js
+++ codefactory_js/3_all_about_object/5_prototype_chain.js
@@ -32,3 +32,43 @@ console.log(testObj.__proto__ === Object.prototype);
 console.log(IdolModel.__proto__ === Function.prototype);
 console.log(Function.prototype.__proto__ === Object.prototype);
 console.log(IdolModel.prototype.__proto__ === Object.prototype);
+
+console.log(yuJin.toString());
+console.log(Object.prototype.toString());
+
+function IdolModel2(name, year){
+    this.name = name;
+    this.year = year;
+
+    this.sayHello = function(){
+        return `${this.name}이 인사를 합니다.`
+    }
+}
+
+const yuJin2 = new IdolModel2('안유진', 2003);
+const wonYoung2 = new IdolModel2('장원영', 2002);
+
+console.log(yuJin2.sayHello());
+console.log(wonYoung2.sayHello());
+console.log(yuJin2.sayHello === wonYoung2.sayHello);
+
+console.log(yuJin2.hasOwnProperty('sayHello'));
+
+function IdolModel3(name, year){
+    this.name = name;
+    this.year = year;
+}
+
+IdolModel3.prototype.sayHello = function(){
+    return `${this.name}이 인사를 합니다.`;
+}
+
+const yuJin3 = new IdolModel3('안유진', 2003);
+const wonYoung3 = new IdolModel3('장원영', 2004);
+
+console.log(yuJin3.sayHello());
+console.log(wonYoung3.sayHello());
+
+console.log(yuJin3.sayHello === wonYoung3.sayHello);
+
+console.log(yuJin3.hasOwnProperty('sayHello'));
