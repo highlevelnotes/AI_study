9e4cdeaec246194ce81e13a473d9cfc91341f2e6
highlevelnotes <pneuco153@gmail.com>
2026-07-05 14:18:19 +0900

    update all

--- codefactory_js/3_all_about_object/5_prototype_chain.js
+++ codefactory_js/3_all_about_object/5_prototype_chain.js
@@ -72,3 +72,81 @@ console.log(wonYoung3.sayHello());
 console.log(yuJin3.sayHello === wonYoung3.sayHello);
 
 console.log(yuJin3.hasOwnProperty('sayHello'));
+
+IdolModel3.sayStaticHello = function(){
+    return '안녕하세요 저는 static method 입니다.'
+}
+
+console.log(IdolModel3.sayStaticHello());
+
+/**
+ * Overriding
+ */
+function IdolModel4(name, year){
+    this.name = name;
+    this.year = year;
+
+    this.sayHello = function(){
+        return '안녕하세요 저는 인스턴스 메서드입니다!'
+    }
+}
+
+IdolModel4.prototype.sayHello = function(){
+    return '안녕하세요 저는 prototype 메서드입니다!'
+}
+
+const yuJin4 = new IdolModel4('안유진', 2003);
+
+// 프로퍼티 섀도잉 - class에서 override
+console.log(yuJin4.sayHello());
+
+/**
+ * getPrototypeOf, setPrototypeOf
+ * 
+ * 인스턴스의 __proto__ 변경 vs 함수의 prototype 변경
+ */
+function IdolModel(name, year){
+    this.name = name;
+    this.year = year;
+}
+
+IdolModel.prototype.sayHello = function(){
+    return `${this.name} 인사를 합니다.`
+}
+
+function FemaleIdolModel(name, year){
+    this.name = name;
+    this.year = year;
+
+    this.dance = function(){
+        return `${this.name} 춤을 춥니다.`
+    }
+}
+
+const gaEul = new IdolModel('가을', 2004);
+const ray = new FemaleIdolModel('레이', 2004);
+
+console.log(gaEul.__proto__);
+console.log(gaEul.__proto__ === IdolModel.prototype);
+console.log(Object.getPrototypeOf(gaEul) === IdolModel.prototype);
+
+console.log(gaEul.sayHello());
+console.log(ray.dance());
+console.log(Object.getPrototypeOf(ray) === FemaleIdolModel.prototype);
+// console.log(ray.sayHello());
+
+Object.setPrototypeOf(ray, IdolModel.prototype);
+console.log(ray.sayHello());
+
+console.log(ray.constructor === FemaleIdolModel);
+console.log(ray.constructor === IdolModel);
+console.log(gaEul.constructor === IdolModel);
+console.log(Object.getPrototypeOf(ray) === FemaleIdolModel.prototype);
+console.log(Object.getPrototypeOf(ray) === IdolModel.prototype);
+console.log(FemaleIdolModel.prototype === IdolModel.prototype);
+
+FemaleIdolModel.prototype = IdolModel.prototype;
+
+const eSeo = new FemaleIdolModel('이서', 2007);
+console.log(Object.getPrototypeOf(eSeo) === FemaleIdolModel.prototype);
+console.log(FemaleIdolModel.prototype === IdolModel.prototype);
